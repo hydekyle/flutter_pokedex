e55a490c0ffa07b5cb7155681df31d66318097fe
hydekyle <hydekyle@hotmail.com>
2024-05-05 20:34:07 +0100

    all working

--- lib/ui/pages/captured_page/captured_controller.dart
+++ lib/ui/pages/captured_page/captured_controller.dart
@@ -1,12 +1,10 @@
 import 'dart:collection';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_pokedex/models/dto/pokemon/pokemon.dart';
 import 'package:flutter_pokedex/models/dto/pokemon_data/pokemon_data.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import '../../../models/dto/pokemon_generation/pokemon_generation.dart';
-import '../pokedex_page/pokedex_controller.dart';
 
 class CapturedController extends GetxController {
   final PokemonGeneration generation;
@@ -33,6 +31,8 @@ class CapturedController extends GetxController {
         capturedList.add(pokemon);
       }
     }
+
+    capturedList.listen((p0) => changeThemeByCaputedList());
   }
 
   addToCapturedList(int pokemonID) {
@@ -101,6 +101,8 @@ class CapturedController extends GetxController {
   }
 
   changeThemeByCaputedList() async {
+    if (capturedList.isEmpty) return;
+
     List<PokemonData> capturedPokemonDataList =
         await getPokemonDataListByPokemonList(capturedList);
 
@@ -115,8 +117,6 @@ class CapturedController extends GetxController {
         } else {
           totalTypeAmountMap[typeName] = 1;
         }
-
-        //pokemonTypes.add(capturedPokemonDataList[x].typeSlots[y].type);
       }
     }
 
@@ -126,20 +126,57 @@ class CapturedController extends GetxController {
     final isMaxValueDraw =
         myList.where((test) => test.value == maxValue).toList().length >= 2;
 
-    print("Empate de valores: $isMaxValueDraw");
-
-    Get.changeTheme(ThemeData.dark());
-
-    myList.forEach((action) => print("${action.key}:${action.value}"));
+    if (isMaxValueDraw) {
+      Get.changeTheme(getThemeData());
+    } else {
+      Get.changeTheme(
+          getThemeData(themeColor: getColorByPokemonType(myList.last.key)));
+    }
   }
 
-  MaterialColor getColorsByPokemonType(String typeName) {
+  Color getColorByPokemonType(String typeName) {
     switch (typeName) {
+      case "normal":
+        return Colors.grey;
+      case "fighting":
+        return Colors.brown.shade700;
+      case "flying":
+        return Colors.blueGrey;
+      case "poison":
+        return Colors.deepPurple;
+      case "ground":
+        return Colors.orange;
+      case "rock":
+        return Colors.brown;
       case "bug":
         return Colors.pink;
+      case "ghost":
+        return Colors.deepPurple.shade800;
+      case "fire":
+        return Colors.red;
+      case "water":
+        return Colors.blue;
+      case "grass":
+        return Colors.green;
+      case "electric":
+        return Colors.amber;
+      case "psychic":
+        return Colors.pink;
+      case "ice":
+        return Colors.cyan;
+      case "dragon":
+        return Colors.purple;
       case _:
         return Colors.deepOrange;
     }
+  }
+
+  ThemeData getThemeData({Color? themeColor}) {
+    return ThemeData(
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: themeColor ?? Colors.deepOrange,
+      ),
+    );
   }
 
   Future<List<PokemonData>> getPokemonDataListByPokemonList(
